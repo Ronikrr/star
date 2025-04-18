6c8ff71686f736d4834a6cf6b9919394b1601627
websolex <102733678+Ronikrr@users.noreply.github.com>
2025-04-18 14:41:48 +0530

    first commit "

---
+++ src/components/OwnerSection.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { FaWhatsapp } from "react-icons/fa";
+const team = [
+    {
+        id: 1,
+        img: '',
+        name: '', post: '', whatsapp: ''
+    }, {
+        id: 2,
+        img: '',
+        name: '', post: '', whatsapp: ''
+    }, {
+        id: 3,
+        img: '',
+        name: '', post: '', whatsapp: ''
+    }
+]
+const OwnerSection = () => {
+    return (
+        <div className='py-[70px]' >
+            <div className="container mx-auto">
+                <div className="w-full text-center mb-7">
+                    <h1 className='text-4xl font-bold' >Meet Our Team</h1>
+                </div>
+                <div className="flex flex-wrap mx-auto justify-center w-[75%]">
+                    {team.map((item, index) => (
+                        
+                        <div className="w-full md:w-6/12 lg:w-4/12 ">
+                            <div className="px-[1rem] w-full relative ">
+                                <div className="card mb-[30px] bg-[#fff] border border-[rgba(0,0,0,.175)] flex-col flex ">
+                                    <div className="w-full h-auto overflow-hidden rounded">
+                                        <img src="" className='object-contain w-full h-full aspect-square ' alt="" />
+                                    </div>
+                                    <div className="py-[24px] text-center">
+                                        <h1 className='mb-1 capitalize text-[22px] font-semibold ' >name</h1>
+                                        <p className='mb-1 capitalize text-16' >post</p>
+                                        <div className="flex items-center justify-center">
+                                            <a href='#' className="w-[50px] h-[50px] rounded-lg bg-gray-100  flex justify-center border border-gray-400  items-center ">
+                                                <FaWhatsapp className='text-2xl ' />
+                                            </a>
+    
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OwnerSection
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Aboutbanner from '../components/Banner'
 import aboutbannerimage from '../assets/image/about_page/about-us-hero-image.jpeg'
 import OurGuarantee from '../components/OurGuarantee'
+import OwnerSection from '../components/OwnerSection'
 
 const About = () => {
   return (
       <div>
       <Aboutbanner title={"about page"} heroImage={aboutbannerimage} />
-      <OurGuarantee/>
+      <OurGuarantee />
+      <OwnerSection />
       </div>
   )
 }
