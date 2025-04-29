d633401ee6fa496e04b46b4c8a0c117f2549d59f
websolex <102733678+Ronikrr@users.noreply.github.com>
2025-04-29 16:56:49 +0530

    change

--- src/components/InquiryForm.jsx
+++ src/components/InquiryForm.jsx
@@ -1,101 +1,152 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 import { countryList } from '../utils/countrylist.utils';
+import emailjs from '@emailjs/browser';
+const initalState = {
+    firstname: '',
+    lastname: '',
+    phoneno: '',
+    email: "",
+    companyname: '',
+    diamondtype: '',
+    country: "",
+    message: ''
+}
 const InquiryForm = () => {
+    const [FromData, setFromData] = useState(initalState)
+
+    const onChange = (e) => {
+        const { name, value } = e.target
+        setFromData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(FromData)
+        emailjs.send('service_csia6iy', 'template_confirmation', FromData, 'NuQv9XskxV05oXLmu')
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                alert("Form Submited Successfully!")
+                setFromData(initalState)
+
+            }, (err) => {
+                console.error('FAILED...', err);
+                alert("FAILED")
+            });
+    }
+    const handlePhoneChange = (value) => {
+        setFromData((prev) => ({
+            ...prev,
+            phoneno: value,
+        }));
+    };
     return (
-        <form
+        <form onSubmit={onSubmit}
             className="grid gap-5 py-10 bg-white sm:grid-cols-2 sm:place-content-center max-md:px-6 lg:px-6"
 
         >
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-name">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="firstname">
                     First Name
                 </label>
                 <input
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     type="text"
-                    name="name"
-                    id="inp-name"
+                    name="firstname"
+                    value={FromData.firstname}
+                    onChange={onChange}
+                    id="firstname"
                     placeholder="Enter your first name"
 
                 />
 
             </div>
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-surname">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="lastname">
                     Last Name
                 </label>
                 <input
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     type="text"
-                    name="surname"
-                    id="inp-surname"
+                    name="lastname"
+                    value={FromData.lastname}
+                    onChange={onChange}
+                    id="lastname"
                     placeholder="Enter your last name"
 
                 />
 
             </div>
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-phone">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="phoneno">
                     Phone
                 </label>
                 <PhoneInput
-                    id="inp-phone"
+                    id="phoneno"
                     defaultCountry="US"
                     focusInputOnCountrySelection={false}
-
                     international
+                    value={FromData.phoneno}
+                    onChange={handlePhoneChange}
                     countryCallingCodeEditable={false}
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem]  !px-2 !outline-none focus:!outline-none"
                 />
             </div>
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-email">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="email">
                     Email
                 </label>
                 <input
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     type="text"
                     name="email"
-                    id="inp-email"
+                    value={FromData.email}
+                    onChange={onChange}
+                    id="email"
                     placeholder="Enter your email"
                 />
             </div>
 
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-companyname">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="companyname">
                     Company Name
                 </label>
                 <input
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     type="text"
-                    name="company-name"
-                    id="inp-companyname"
+                    value={FromData.companyname}
+                    onChange={onChange}
+                    name="companyname"
+                    id="companyname"
                     placeholder="Enter your company name"
                 />
             </div>
 
             <div className="grid gap-[.25rem]">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-diamond-type">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="diamondtype">
                     Diamond Type
                 </label>
                 <div>
                     <select
-                        id="inp-diamond-type"
-                        name="diamond-type"
+                        id="diamondtype"
+                        name="diamondtype"
+                        value={FromData.diamondtype}
+                        onChange={onChange}
                         className="w-full rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     >
                         <option disabled value="">
                             Select Diamond Type
                         </option>
-                        <option value={""}>
+                        <option value={" Natural Diamonds"}>
                             Natural Diamonds
                         </option>
-                        <option value={""}>
+                        <option value={"Lab Grown Diamonds (CVD)"}>
                             Lab Grown Diamonds (CVD)
                         </option>
-                        <option value={""}>
+                        <option value={"Lab Grown Diamonds (HPHT)"}>
                             Lab Grown Diamonds (HPHT)
                         </option>
                     </select>
@@ -103,15 +154,16 @@ const InquiryForm = () => {
             </div>
 
             <div className="grid gap-[.25rem] sm:col-span-2">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-country">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="country">
                     Country
                 </label>
                 <div>
                     <select
-                        id="inp-country"
+                        id="country"
                         name="country"
                         className="w-full rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
-
+                        value={FromData.country}
+                        onChange={onChange}
                     >
                         <option disabled value="">
                             Select Country
@@ -126,13 +178,15 @@ const InquiryForm = () => {
                 </div>
             </div>
             <div className="grid gap-[.25rem] sm:col-span-2">
-                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="inp-message">
+                <label className="ml-3 text-14 leading-[1.25rem] font-semibold text-primary  " htmlFor="message">
                     Message
                 </label>
                 <textarea
                     className="rounded-[1rem] border border-primary/20 px-[1rem] py-[0.75rem] "
                     name="message"
-                    id="inp-message"
+                    id="message"
+                    value={FromData.message}
+                    onChange={onChange}
                     placeholder="Describe your message..."
                     rows={7}
 
